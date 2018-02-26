af313e341213b760094097a7c06e02bb8998bc96
Liz House <lizehouse@gmail.com>
2018-02-26 12:10:19 -0700

    create pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :pages # resources :name_of_controller <= should be plural
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
